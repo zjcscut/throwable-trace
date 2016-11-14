36b19cd8adc38db6941861f9b404c905799428f2
zhangjinci <15118850470@163.com>
2016-11-14 20:28:04 +0800

    zjc: 完善hibernate动态参数和依赖配置

---
+++ throwable-orm/src/main/java/org/throwable/trace/orm/mybatis/MybatisAutoConfiguration.java
@@ -0,0 +1,21 @@
+package org.throwable.trace.orm.mybatis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhangjinci
+ * @version 2016/11/14 19:16
+ * @function Mybatis自动配置
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {"org.throwable.trace.orm.mybatis"}
+)
+public class MybatisAutoConfiguration {
+
+
+    @Bean
+    public
+}
---
+++ throwable-orm/src/main/java/org/throwable/trace/orm/throwjdbc/ThrowJdbcAutoConfiguration.java
@@ -0,0 +1,16 @@
+package org.throwable.trace.orm.throwjdbc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhangjinci
+ * @version 2016/11/14 18:00
+ * @function  对Spring JDBC进行封装 ,自动配置并进行模块扫描
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {"org.throwable.trace.orm.throwjdbc"}
+)
+public class ThrowJdbcAutoConfiguration {
+}
